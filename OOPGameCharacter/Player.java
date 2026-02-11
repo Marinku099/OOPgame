04835bdee25973f95502625bdb67e1e25ce573a8
ppetxaii <68050396@kmitl.ac.th>
2026-02-11 23:43:33 +0700

    summary
    ตอนนี้เขียนตาม UML เสร็จแล้ว ซึ่งมันจะแดงตรงบางบรรทัด ซึ่งจะแดงตรง ClothingItem เป็นหลัก

--- OOPGameCharacter/Player.java
+++ OOPGameCharacter/Player.java
@@ -1,5 +1,50 @@
 package OOPGameCharacter;
+import java.util.ArrayList;
+
+import GameItem.ClothingItem;
 
 public class Player {
+    private int balance;
+    private int luck;
+    private ArrayList<ClothingItem> items;
+
+    public Player(int balance, int luck) {
+        this.balance = balance;
+        this.luck = luck;
+        this.items = new ArrayList<>();
+    }
     
+    //ส่งข้อเสนอไปให้ item
+    public void sentOfferTo(NPC npc, ClothingItem item) {
+        npc.receiveOffer(this,item);
+    }
+
+    //ซื้อของจาก Seller
+    public void buyItemFrom(Seller seller) {
+        ClothingItem item = seller.sellItem();
+
+        if (item != null) {
+           int price = (int) item.calculateRealValue();
+           if (balance >= price) {
+            balance -= price;
+            items.add(item);
+           }
+        }
+    }
+
+    //ขายของให้ Buyer
+    public void sellItemTo(Buyer buyer) {
+        if (!items.isEmpty()) {
+            ClothingItem item = items.remove(0);
+            balance += buyer.buyItem(item);
+        }
+    }
+
+    private void setBalance(int n) {
+        this.balance = n;
+    }
+
+    private void setLuck(int n) {
+        this.luck = n;
+    }
 }

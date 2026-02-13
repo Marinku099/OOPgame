c5f8c2f2bc5a812c7cdc5cb5099524fb6422da2c
wa21-Wa <68050037@kmitl.ac.th>
2026-02-13 17:03:39 +0700

    UpdateClothingType

--- Type/ClothingType.java
+++ Type/ClothingType.java
@@ -3,13 +3,19 @@ package Type;
 
 public enum ClothingType {
     Camisole(50),
-    CropTop(120),
+    Tops(120),
+    Blouse(200),
     T_SHIRT(200),
     Shirt(200),
     Polo(250),
+    Vest(330),
     Sweater(350),
+    Cardigan(350),
+    Tracktop(400),
     HOODIE(450),
-    JACKET(550);
+    JACKET(550),
+    Dress(470),
+    Coat(500);
 
     private final double defaultBasePrice;
 

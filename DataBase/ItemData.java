5fa056d71b4935b831da561a4a30dc1971f6d76c
wa21-Wa <68050037@kmitl.ac.th>
2026-02-14 22:10:04 +0700

    UpdateDataBase

--- DataBase/ItemData.java
+++ DataBase/ItemData.java
@@ -8,11 +8,13 @@ public class ItemData {
     public ClothingType type;
     public Rarity rarity;
     public String description;
+    public String imagepath;
 
-    public ItemData(String name, ClothingType type, Rarity rarity, String description) {
+    public ItemData(String name, ClothingType type, Rarity rarity, String description, String imagepath) {
         this.name = name;
         this.type = type;
         this.rarity = rarity;
         this.description = description;
+        this.imagepath = imagepath;
     }
 }
